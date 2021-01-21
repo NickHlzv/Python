61c2dce049e94214657bb5f460916b5b43396053
NickHlzv <nikitos.halzev@gmail.com>
2021-01-22 00:05:10 +0300

    Первое задание готово

--- Lesson1.Homework/task1.py
+++ Lesson1.Homework/task1.py
@@ -2,6 +2,8 @@ int1 = 12
 float1 = 1.4256
 str1 = 'И снова здравствуй'
 str2 = input('Введите Имя')
-int2 = input('Введите число')
-
-print('{}, {}, {}, {}, {}'.format(int1, float1, str1, str2, int2))
+int2 = input('Введите целое число')
+if int2.isdigit():
+    print('{}, {}, {}, {}, {}'.format(int1, float1, str1, str2, int(int2)))
+else:
+    print('Ну елки зеленые просил же целое число')
