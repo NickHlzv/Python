f41a150d134828f468fa7d73241e7380064cbb71
NickHlzv <nikitos.halzev@gmail.com>
2021-01-21 23:09:54 +0300

    Первое задание готово

---
+++ Lesson1.Homework/task1.py
@@ -0,0 +1,7 @@
+int1 = 12
+float1 = 1.4256
+str1 = 'И снова здравствуй'
+str2 = input('Введите Имя')
+int2 = input('Введите число')
+
+print('{}, {}, {}, {}, {}'.format(int1, float1, str1, str2, int2))
