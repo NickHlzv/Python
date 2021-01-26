f217d6ee00a048d78cd478a3eaceadd37eed290a
NickHlzv <nikitos.halzev@gmail.com>
2021-01-26 18:06:51 +0300

    Второе задание готово

---
+++ Lesson2.Homework/task2.py
@@ -0,0 +1,15 @@
+count = input('Введите размер списка: ')
+while not count.isdigit():
+    count = input('Размер списка может быть только целым числом. Прошу нормально вводить: ')
+else:
+    count = int(count)
+lst = []
+for el in range(1, count+1):
+    lst_el = input(f'Введите {el}й элемент списка: ')
+    lst.append(lst_el)
+print(lst)
+if count % 2 != 0:
+    count -= 1
+for i in range(0, count, 2):
+    lst[i], lst[i+1] = lst[i+1], lst[i]
+print(lst)
