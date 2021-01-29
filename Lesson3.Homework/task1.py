53a65529c3d160039c92e1d5dc4c13464bf0d1f2
NickHlzv <nikitos.halzev@gmail.com>
2021-01-29 12:11:09 +0300

    Первое задание готово

--- Lesson3.Homework/task1.py
+++ Lesson3.Homework/task1.py
@@ -2,4 +2,9 @@ def pair_division(a, b):
     return a / b if b != 0 else 'Мы на 0 не делим'
 
 
-print(pair_division(15, 0))
+a = input('Введите число a')
+b = input('Введите число b')
+if a.isdigit() and b.isdigit():
+    print(pair_division(int(a), int(b)))
+else:
+    print('Числа должны быть целыми, попробуйте еще')
