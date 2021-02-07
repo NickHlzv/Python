d12d9a03b92d75695ad2b9fe1040bb76b6dff673
NickHlzv <nikitos.halzev@gmail.com>
2021-02-07 22:43:13 +0300

    Седьмое задание готово

---
+++ Lesson5.Homework/task7.py
@@ -0,0 +1,17 @@
+import json
+with open('file7.txt', 'r', encoding='utf-8') as f:
+    firms = {}
+    average = 0
+    count = 0
+    for line in f:
+        gain = float(line.split()[2])
+        costs = float(line.split()[3])
+        firms.update({line.split()[0]: gain - costs})
+        if costs < gain:
+            average += gain - costs
+            count += 1
+    average = average/count
+    avg_dict = {'average_profit': average}
+    result = [firms, avg_dict]
+with open('data7.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f)
